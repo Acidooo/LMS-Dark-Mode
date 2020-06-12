f0c71a971e05114c95eb05d6a88fdd5716955bd8
Acido <15827009+Acidooo@users.noreply.github.com>
2020-06-12 19:59:32 +0300

    Update LMS KTUN Dark Mode.user.js

--- LMS KTUN Dark Mode.user.js
+++ LMS KTUN Dark Mode.user.js
@@ -13,16 +13,6 @@
 // ==/UserScript==
 /*jshint esversion: 6 */
 
-try{
-    var button = document.createElement("Button");
-    button.innerHTML = "Title";
-    button.style = "top:0;right:0;position:absolute;z-index: 9999";
-    document.body.appendChild(button);}
-catch(err){
-    console.log(err);
-}
-
-
 GM_addStyle(`
 
 ul.weeks li.section .content {
