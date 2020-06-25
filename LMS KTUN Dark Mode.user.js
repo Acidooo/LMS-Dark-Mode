44da32c928c4a2dfc821daa703fc93e58d031571
Acidooo <15827009+Acidooo@users.noreply.github.com>
2020-06-25 20:39:09 +0300

    v1.1.11
    profil ve ödev yükleme sayfaları

--- LMS KTUN Dark Mode.user.js
+++ LMS KTUN Dark Mode.user.js
@@ -2,7 +2,7 @@
 // @name         LMS KTUN Dark Mode
 // @namespace    https://github.com/Acidooo/LMS-Dark-Mode
 // @icon         https://lms.ktun.edu.tr/pluginfile.php/1/theme_remui/faviconurl/1589613162/imageedit_1_7336125810.png
-// @version      1.1.10
+// @version      1.1.11
 // @author       Acido
 // @description   Dark Mode for lms.ktun.edu.tr
 // @include      *://lms.ktun.edu.tr/*
@@ -22,12 +22,14 @@ GM_addStyle(`
 .filemanager-toolbar { background-color: #999; }
 .fp-iconview .fp-filename-field .fp-filename {background: #fff0;}
 
+
 /*odev*/
 .path-mod-assign div.submissionsummarytable table tbody tr td.c0 {background-color: #444;color: #dadada;}
 #page-admin-course-index .editcourse tbody td, .forumheaderlist tbody td, .generaltable tbody td, form#movecourses table tbody td, table#defineroletable tbody td, table#explaincaps tbody td, table#listdirectories tbody td, table.collection tbody td, table.flexible tbody td, table.grading-report tbody td, table.rolecaps tbody td, table.userenrolment tbody td {     background-color: #555;       color: #dadada; }
 .path-mod-assign td.submissionstatussubmitted, .path-mod-assign div.submissionstatussubmitted, .path-mod-assign a:link.submissionstatussubmitted {    background-color: #cfefcf;  }
 .path-mod-assign td.submissiongraded, .path-mod-assign div.submissiongraded { }
 .path-mod-assign td.earlysubmission, .path-mod-assign div.earlysubmission {}
+
 
 /*quiz*/
 table.quizreviewsummary td.cell {    background: #444!important;    color: #e4eaec;}
@@ -40,19 +42,19 @@ table.quizreviewsummary th.cell {    background: #333!important;    color: #daea
 .radio-custom label::after { margin-left: 20px; }
 #page-admin-course-index .editcourse tbody tr:nth-of-type(odd), .forumheaderlist tbody tr:nth-of-type(odd), .generaltable tbody tr:nth-of-type(odd), .table-striped tbody tr:nth-of-type(odd), form#movecourses table tbody tr:nth-of-type(odd), table#defineroletable tbody tr:nth-of-type(odd), table#explaincaps tbody tr:nth-of-type(odd), table#listdirectories tbody tr:nth-of-type(odd), table.collection tbody tr:nth-of-type(odd), table.flexible tbody tr:nth-of-type(odd), table.grading-report tbody tr:nth-of-type(odd), table.rolecaps tbody tr:nth-of-type(odd), table.userenrolment tbody tr:nth-of-type(odd) {
     background-color: #444;
-    color: #dadada;
-}
+    color: #dadada;  }
 #page-admin-course-index .editcourse tbody td a, .forumheaderlist tbody td a, .generaltable tbody td a, form#movecourses table tbody td a, table#defineroletable tbody td a, table#explaincaps tbody td a, table#listdirectories tbody td a, table.collection tbody td a, table.flexible tbody td a, table.grading-report tbody td a, table.rolecaps tbody td a, table.userenrolment tbody td a {
     color: #ff3838;
 }
 .mr-5 {    background-color: #f96868;    color: #fff; }
 #page-mod-quiz-view .generalbox#feedback h3 {color: #f96868;}
-
+table.quizattemptsummary .noreviewmessage {    color: #dadada; }
 
 .card-header:first-child { background-color: #444; }
 ul.weeks li.section .content { background-color: #444; }
 .p-0{background-color:none;}
 body { color: #dadada; }
+
 
 /*dropdown message*/
 a.list-group-item:focus, a.list-group-item:hover{ background-color: #555; }
@@ -67,9 +69,11 @@ a.list-group-item { color: #d6d4d4; }
 .dropdown-item {color: #f3f7f9;}
 .navbar-right .dropdown-menu { background-color:#888}
 
+
 /*kayıt*/
 .panel>:not(.panel-loading):not(.collapsing) { background-color: #666; }
 .site-menubar-unfold .page, .site-menubar-unfold .site-footer { background-color: #555; }
+
 
 /*sag container*/
 .site-sidebar-nav.nav-tabs>.nav-item>.nav-link { background-color: #444; }
@@ -81,6 +85,7 @@ a.list-group-item { color: #d6d4d4; }
        background-color: #777;
 }
 
+
 /*takvim*/
 .custom-select { background-color: #999; }
 
@@ -88,7 +93,7 @@ a.list-group-item { color: #d6d4d4; }
 .bg-white { background-color: #555; }
 .bg-grey-100 { background-color: #555!important; }
 .pagelayout-frontpage .page-content h2 { color: #ff3838; }
-.forumpost .content { background: #666; }
+.forumpost .content { background: #777; }
 
 
 /*Mesaj*/
@@ -101,20 +106,18 @@ a.list-group-item { color: #d6d4d4; }
 .messaging-area-container * { color: #fff; }
 .chat-left .chat-content { background-color: #3690c5; }
 .messaging-area-container .messaging-area .contacts-area .searchtextarea .form-control { background-color: #444444; }
-
-
 .response.messaging{ background-color: #444; }//
 
-/*Collapsed sides*/
 
+/*Collapsed sides*/
 .navbar>.container-fluid .navbar-collapse, .navbar>.containers .navbar-collapse { background-color: #555; }
 .site-menubar-body { background-color: #444; }
 .site-menubar-fold .site-navbar .navbar-container { background-color: #555; }
 .site-menubar-fold .page, .site-menubar-fold .site-footer { background-color: #555; }
 .site-menubar-fold .site-menubar { background-color: #555; }
 
-/*Ders Sayfasi*/
 
+/*Ders Sayfasi*/
 .collapse navbar-collapse .navbar-collapse-toolbar { background-color: #666; }
 .no-overflow { color: #dadada; font-weight: bold; }
 h4.sectionname a { color: #ff3838; }
@@ -122,6 +125,7 @@ h4.sectionname a { color: #ff3838; }
 .section .activity.list-group-item:first-child { background-color: #444; }
 //.p-0 { background-color: #666; }
 .p-10 .course-cover-image .pb-30{ background-image: none; background-color: #4440; }
+
 
 /*anasayfa*/
 .page-header+.page-content{ background-color: #555; }
@@ -139,11 +143,13 @@ h4.sectionname a { color: #ff3838; }
 .w-full { background-color: #444; }
 .d-block { display: contents !important; background-color: #444 !important; }
 
+
 /*sol container*/
 .scrollable-vertical.is-enabled .scrollable-container { background-color: #444; }
 .site-menubar-unfold .site-menu>.site-menu-item>a .site-menu-title { color: #fff; }
 .site-menubar-light .site-menubar-footer>a { background-color: #333; }
-.site-menubar-light .site-menubar-footer>a:hover { background-color: #666; } 
+.site-menubar-light .site-menubar-footer>a:hover { background-color: #666; }
+
 
 /*nav-bar*/
 .navbar-default .navbar-toolbar .nav-link { color: #ffffff; }
@@ -154,17 +160,53 @@ h4.sectionname a { color: #ff3838; }
 /*footer*/
 .bg-primary-600{ background-color: #555!important; }
 
+
 /*mobile*/
 .d-block { background-color: #444; }
 
-/*User Profile*/
 
+/*User Profile*/
+.nav-tabs-line .nav-link {    color: #dadada; }
+[role=button], a, area, button, input, label, select, summary, textarea {    color: #fff; }
+.mform .form-inline .custom-select, .mform .form-inline .form-control {    background-color: #dadada; }
+.ftoggler {    background: #666; }
 #page-enrol-users .page-profile #filterform, .page-profile #page-enrol-users #filterform, .page-profile .card-block, .page-profile .userprofile .profile_tree section, .page-profile .well, .page-profile .yui-skin-sam .yui-layout.path-mod-chat-gui_ajax #chat-messages .chat-message.course-theme, .page-profile .yui-skin-sam .yui-layout.path-mod-chat-gui_ajax .yui-layout-unit-bottom, .page-profile .yui-skin-sam .yui-layout.path-mod-chat-gui_ajax .yui-layout-unit-right, .userprofile .profile_tree .page-profile section, .yui-skin-sam .yui-layout.path-mod-chat-gui_ajax #chat-messages .page-profile .chat-message.course-theme, .yui-skin-sam .yui-layout.path-mod-chat-gui_ajax .page-profile .yui-layout-unit-bottom, .yui-skin-sam .yui-layout.path-mod-chat-gui_ajax .page-profile .yui-layout-unit-right {
-    background-color: #888;
-}
+background-color: #888; }
 .page-profile .page-content .list-group-item {    background-color: #444; }
 .page-profile .card-footer { background-color: #333; }
-.additional-actions .col-12 .moodle-actionmenu, .additional-actions .col-12 a {background-color: #999;color: #ffffff;}
+.additional-actions .col-12 .moodle-actionmenu, .additional-actions .col-12 a {background-color: #9990; color: #ffffff;}
+.show>.dropdown-menu {    background-color: #999999a6;  }
+.dropdown-item:focus, .dropdown-item:hover {    background-color: #7770; color:#333 }
+.action-menu .dropdown-toggle {    background-color: #f96868; }
+.wb-settings:before {    color: #fff; }
+.h1, .h2, .h3, h1, h2, h3 {    color: #dadada; }
+
+
+/*forum cevaplari*/
+.forumpost #course-category-listings .content-mask, .forumpost #course-category-listings .options, .forumpost #course-category-listings .topic, .forumpost .row .content-mask, .forumpost .row .options, .forumpost .row .topic {
+    background-color: #666666;
+}
+
+/*Odev gonderim penceresi*/
+//#page-grade-grading-manage .actions .action, #rubric-rubric.gradingform_rubric #rubric-criteria .criterion .addlevel input, #rubric-rubric.gradingform_rubric .addcriterion, .btn, .qnbutton, .section-modchooser-link a {  background-color: #f96868; }
+button, input {    border-radius: 15px; background-color: #efefef; color: black; }
+.moodle-dialogue-base .moodle-dialogue-wrap .moodle-dialogue-hd.yui3-widget-hd { background-color: #656565; }
+.modal-body, .moodle-dialogue-base .moodle-dialogue .moodle-dialogue-bd { background-color: #999; }
+.filemanager .yui3-datatable-even .yui3-datatable-cell { color: #797979; }
+.fp-filename {color: #6b6b6b; }
+.fp-content-center { background-color: #777; }
+.form-control { border-radius: 20px;    background-color: #dadada; }
+select.form-control:not([size]):not([multiple]) { background-color: #dadada; }
+#page-grade-grading-manage .actions .btn-group>.action:first-child:not(:last-child):not(.dropdown-toggle), #rubric-rubric.gradingform_rubric #rubric-criteria .criterion .addlevel .btn-group>input:first-child:not(:last-child):not(.dropdown-toggle), #rubric-rubric.gradingform_rubric .btn-group>.addcriterion:first-child:not(:last-child):not(.dropdown-toggle), .btn-group>.btn:first-child:not(:last-child):not(.dropdown-toggle), .btn-group>.qnbutton:first-child:not(:last-child):not(.dropdown-toggle), .section-modchooser-link .btn-group>a:first-child:not(:last-child):not(.dropdown-toggle) { color: #444; }
+#page-grade-grading-manage .actions .btn-group>.action:not(:first-child):not(:last-child):not(.dropdown-toggle), #rubric-rubric.gradingform_rubric #rubric-criteria .criterion .addlevel .btn-group>input:not(:first-child):not(:last-child):not(.dropdown-toggle), #rubric-rubric.gradingform_rubric .btn-group>.addcriterion:not(:first-child):not(:last-child):not(.dropdown-toggle), .btn-group>.btn:not(:first-child):not(:last-child):not(.dropdown-toggle), .btn-group>.qnbutton:not(:first-child):not(:last-child):not(.dropdown-toggle), .section-modchooser-link .btn-group>a:not(:first-child):not(:last-child):not(.dropdown-toggle) { color: #444;}
+#page-grade-grading-manage .actions .btn-group>.action:last-child:not(:first-child), #rubric-rubric.gradingform_rubric #rubric-criteria .criterion .addlevel .btn-group>input:last-child:not(:first-child), #rubric-rubric.gradingform_rubric .btn-group>.addcriterion:last-child:not(:first-child), .btn-group>.btn:last-child:not(:first-child), .btn-group>.dropdown-toggle:not(:first-child), .btn-group>.qnbutton:last-child:not(:first-child), .section-modchooser-link .btn-group>a:last-child:not(:first-child) { color: #444; }
+
+
+
+
+
+
+
 
 `);
 
